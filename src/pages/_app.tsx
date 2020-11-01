e24b7e8b790bb9b2e3f1a4e2fcd359f2c5a5ba8f
Guilhermo Masid <guilhermomasid@gmail.com>
2020-11-01 13:23:36 -0300

    removing authentication

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,4 +1,3 @@
-import { Provider } from 'next-auth/client'
 import React from 'react'
 import { AppProps } from 'next/app'
 import { ThemeProvider } from 'styled-components'
@@ -7,13 +6,9 @@ import GlobalStyle from '~/styles/global'
 import theme from '~/styles/theme'
 
 const MyApp: React.FC<AppProps> = ({ Component, pageProps }) => {
-  const { session } = pageProps
-
   return (
     <ThemeProvider theme={theme}>
-      <Provider session={session}>
-        <Component {...pageProps} />
-      </Provider>
+      <Component {...pageProps} />
       <GlobalStyle />
     </ThemeProvider>
   )
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,52 +1,12 @@
 import React from 'react'
-import NextAuth from 'next-auth/client'
-import { GetServerSideProps } from 'next'
+import { Container } from '~/styles/pages/Home'
 
-interface IUser {
-  name?: string
-  email: string
-  image?: string
-}
-
-interface ISession {
-  accessToken: string
-  expires: string
-  user: IUser
-}
-
-interface SessionProps {
-  session: ISession
-}
-
-const Home: React.FC<SessionProps> = ({ session }) => {
+const Home: React.FC = () => {
   return (
-    <p>
-      {!session && (
-        <>
-          Not signed in <br />
-          <button onClick={() => NextAuth.signin()}>Sign in</button>
-        </>
-      )}
-      {session && (
-        <>
-          Signed in as {session.user.email} <br />
-          <button onClick={() => NextAuth.signout()}>Sign out</button>
-        </>
-      )}
-    </p>
+    <Container>
+      <h1>Initial Structure</h1>
+    </Container>
   )
-}
-
-export const getServerSideProps: GetServerSideProps<SessionProps> = async ({
-  req
-}) => {
-  const session = await NextAuth.session({ req })
-
-  return {
-    props: {
-      session
-    }
-  }
 }
 
 export default Home
