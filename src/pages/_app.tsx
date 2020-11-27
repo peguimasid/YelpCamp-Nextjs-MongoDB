0d8ea906f5373cc788daf5ce17aba95af10e9f12
Guilhermo Masid <guilhermomasid@gmail.com>
2020-11-27 11:21:26 -0300

    removing spinner Nprogress

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -10,6 +10,8 @@ import 'nprogress/nprogress.css'
 import GlobalStyle from '~/styles/global'
 import theme from '~/styles/theme'
 
+NProgress.configure({ showSpinner: false })
+
 Router.events.on('routeChangeStart', () => NProgress.start())
 Router.events.on('routeChangeComplete', () => NProgress.done())
 Router.events.on('routeChangeError', () => NProgress.done())
